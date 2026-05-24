a1c0a16c3a1b769ec513227ffdfb2687ff12eaf9
Suzuna Sato <s25G1059AS@s.chibakoudai.jp>
2026-05-25 02:42:48 +0900

    関数の処理内容まで

--- HCK1_group/HCK1_group_mywork/Display/Display.cpp
+++ HCK1_group/HCK1_group_mywork/Display/Display.cpp
@@ -74,3 +74,40 @@ const uint8_t font3x5[10][15] = {
    1, 1, 1}
 
 };
+
+//LEDMatrixのインスタンス化
+ArduinoLEDMatrix matrix;
+
+
+//関数
+
+void displeyBPM(int bpm){
+  //初期化
+  uint32_t frame[12] = {0}; 
+  matrix.renderBitmap(frame,12,8); 
+
+  drawDugit(bpm,0);
+}
+
+void drawDigit(int digit, int x_offset){
+  if (digit < 0 || digit > 9) return;  //範囲外
+
+  //フレームバッファ 
+  uint32_t frame[4] = {0};
+
+  //font3x5[digit]をframeにマッピング
+  for (int y = 0; y < 5; y++){
+    for (int x = 0; x < 3; x++){
+      if(font3x5[digit][y * 3 + x] == 1){
+        //ビット計算
+      }
+    }
+  }
+
+  matrix.renderBitmap(frame,8,12)
+
+
+
+
+}
+
